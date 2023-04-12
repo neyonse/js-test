6bf27febb4fe3a498eda75c07babbe8c0eb010e2
neyonse <neyonse@gmail.com>
2023-04-12 21:03:39 +0300

    update test_module03.js

--- js/test_module03.js
+++ js/test_module03.js
@@ -78,41 +78,106 @@
 
 //-------------------------------------------------------------------------------------------------------------------
 
+// Просто тестую різні штуки
+
+// const nameVariable = 'Amelia';
+// const age = 22;
+
+// const object01 = {
+//     name: nameVariable,
+//     surname: 'Doe',
+//     age,
+// };
+
+// function getValues(object) {
+//     const values = [];
+
+//     for (const key in object) {
+//         values.push(object[key]);
+//     }
+
+//     return values;
+// }
+
+// console.log(getValues(object01));
+
+// const object02 = Object.create(object01);
+// object02.mail = 'poly@mail.com';
+// object02.isOnline = true;
+// object02.score = 500;
+
+// function countProps(object) {
+//     let propCount = 0;
+
+//     for (const key in object) {
+//         if (object.hasOwnProperty(key)) {
+//             propCount += 1;
+//         }
+//     }
+
+//     return propCount;
+// }
+
+// console.log(object01.name);
+
+// console.log(countProps(object02));
+
+//-------------------------------------------------------------------------------------------------------------------
+
+const apartment = {
+    descr: 'Spacious apartment in the city center',
+    rating: 4,
+    price: 2153,
+};
+const values = [];
+const keys = Object.keys(apartment);
+
+for (let key of keys) {
+    values.push(apartment[key]);
+}
+
+console.log(keys);
+console.log(values);
+
+//-------------------------------------------------------------------------------------------------------------------
+
 // Кошик товарів
 
-const cart = {
-    items: [],
-    getItems() {
-        return this.items;
-    },
-    add(product) {
-        product.quantity = 1;
-    },
-    remove(productName) {},
-    clear() {},
-    countTotalPrice() {},
-    increaseQuantity(productName) {},
-    decreaseQuantity(productName) {},
-};
+// const cart = {
+//     items: [],
+//     getItems() {
+//         return this.items;
+//     },
+//     add(product) {
+//         product.quantity = 1;
+//     },
+//     remove(productName) {},
+//     clear() {},
+//     countTotalPrice() {},
+//     increaseQuantity(productName) {},
+//     decreaseQuantity(productName) {},
+// };
 
-console.table(cart);
-console.table(cart.getItems());
+// console.table(cart);
+// console.table(cart.getItems());
 
-cart.add({ name: 'apple', price: 30 });
-cart.add({ name: 'pear', price: 80 });
-cart.add({ name: 'lemon', price: 35 });
-cart.add({ name: 'zucchini', price: 115 });
+// cart.add({ name: 'apple', price: 30 });
+// cart.add({ name: 'pear', price: 80 });
+// cart.add({ name: 'lemon', price: 35 });
+// cart.add({ name: 'zucchini', price: 115 });
 
-console.table(cart.getItems());
+// console.table(cart.getItems());
 
-cart.remove('lemon');
-console.table(cart.getItems());
+// cart.remove('lemon');
+// console.table(cart.getItems());
 
-cart.clear();
-console.table(cart.getItems());
+// cart.clear();
+// console.table(cart.getItems());
 
-cart.increaseQuantity('apple');
-console.table(cart.getItems());
+// cart.increaseQuantity('apple');
+// console.table(cart.getItems());
 
-cart.decreaseQuantity('pear');
-console.table(cart.getItems());
+// cart.decreaseQuantity('pear');
+// console.table(cart.getItems());
+
+//-------------------------------------------------------------------------------------------------------------------
