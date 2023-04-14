3a67e8a11c56b7dc87f59f6fc58ab18d28ede37f
neyonse <neyonse@gmail.com>
2023-04-14 15:40:59 +0300

    update test_module03.js

--- js/test_module03.js
+++ js/test_module03.js
@@ -124,20 +124,77 @@
 
 //-------------------------------------------------------------------------------------------------------------------
 
-const apartment = {
-    descr: 'Spacious apartment in the city center',
-    rating: 4,
-    price: 2153,
-};
-const values = [];
-const keys = Object.keys(apartment);
+// const colors = [
+//     { hex: '#f44336', rgb: '244,67,54' },
+//     { hex: '#2196f3', rgb: '33,150,243' },
+//     { hex: '#4caf50', rgb: '76,175,80' },
+//     { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
 
-for (let key of keys) {
-    values.push(apartment[key]);
-}
+// const hexColors = [];
+// const rgbColors = [];
 
-console.log(keys);
-console.log(values);
+// for (let color of colors) {
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+//-------------------------------------------------------------------------------------------------------------------
+
+// const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//     for (let product of products) {
+//         if (product.name === productName) {
+//             return product.price;
+//         }
+//     }
+//     return null;
+// }
+
+// console.log(getProductPrice('Radar'));
+// console.log(getProductPrice('Grip'));
+// console.log(getProductPrice('Scanner'));
+// console.log(getProductPrice('Droid'));
+// console.log(getProductPrice('Engine'));
+
+// function getAllPropValues(propName) {
+//     const propValues = [];
+
+//     for (let product of products) {
+//         if (product.hasOwnProperty(propName)) {
+//             propValues.push(product[propName]);
+//         }
+//     }
+//     return propValues;
+// }
+
+// console.log(getAllPropValues('name'));
+// console.log(getAllPropValues('quantity'));
+// console.log(getAllPropValues('price'));
+// console.log(getAllPropValues('category'));
+
+// function calculateTotalPrice(productName) {
+//     let totalPrice = 0;
+//     for (let product of products) {
+//         if (product.name === productName) {
+//             totalPrice = product.price * product.quantity;
+//         }
+//     }
+//     return totalPrice;
+// }
+
+// console.log(calculateTotalPrice('Blaster'));
+// console.log(calculateTotalPrice('Radar'));
+// console.log(calculateTotalPrice('Droid'));
 
 //-------------------------------------------------------------------------------------------------------------------
 
