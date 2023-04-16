1b876bf84547cfcd7d662575b18f939e7809a96a
neyonse <neyonse@gmail.com>
2023-04-17 00:03:36 +0300

    update test_module03.js

--- js/test_module03.js
+++ js/test_module03.js
@@ -198,6 +198,65 @@
 
 //-------------------------------------------------------------------------------------------------------------------
 
+const atTheOldToad = {
+    potions: [
+        { name: 'Speed potion', price: 460 },
+        { name: 'Dragon breath', price: 780 },
+        { name: 'Stone skin', price: 520 },
+    ],
+    getPotions() {
+        return this.potions;
+    },
+    addPotion(newPotion) {
+        for (const potion of this.potions) {
+            if (potion.name === newPotion.name) {
+                return `Error! Potion ${newPotion.name} is already in your inventory!`;
+            }
+        }
+
+        this.potions.push(newPotion);
+        return this.potions;
+    },
+    removePotion(potionName) {
+        for (const potion of this.potions) {
+            if (potion.name === potionName) {
+                const potionIndex = this.potions.indexOf(potion);
+
+                console.log(potionIndex);
+
+                this.potions.splice(potionIndex, 1);
+                return this.potions;
+            }
+        }
+
+        return `Potion ${potionName} is not in inventory!`;
+    },
+    updatePotionName(oldName, newName) {
+        for (const potion of this.potions) {
+            if (potion.name === oldName) {
+                potion.name = newName;
+
+                return this.potions;
+            }
+        }
+
+        return `Potion ${oldName} is not in inventory!`;
+    },
+};
+
+// console.log(atTheOldToad.getPotions());
+
+// console.log(atTheOldToad.addPotion({ name: 'Invisibility', price: 620 }));
+// console.log(atTheOldToad.addPotion({ name: 'Dragon breath', price: 700 }));
+
+// console.log(atTheOldToad.removePotion('Dragon breath'));
+
+// console.log(
+//     atTheOldToad.updatePotionName('Stone skin', 'Invulnerability potion')
+// );
+
+//-------------------------------------------------------------------------------------------------------------------
+
 // Кошик товарів
 
 const cart = {
