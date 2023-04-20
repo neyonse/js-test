f284edaf1ace5f42591d790ff5610061df53d933
neyonse <neyonse@gmail.com>
2023-04-20 18:10:16 +0300

    update names

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-// import './js/test_module01.js';
-// import './js/test_module02.js';
-import './js/test_module03.js';
+// import './js/module01.js';
+// import './js/module02.js';
+// import './js/module03.js';
+import './js/module04.js';
