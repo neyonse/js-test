eb807af9bce5b95081e4aabf4ff57935a40e02ef
neyonse <neyonse@gmail.com>
2023-04-27 19:02:27 +0300

    update module05.js

--- js/module05.js
+++ js/module05.js
@@ -79,3 +79,35 @@
 // updateCounter(10, counter.increment);
 
 //-------------------------------------------------------------------------------------------------------------------
+
+// const historyService = {
+//     orders: [
+//         { email: 'jacob@hotmail.com', dish: 'Burrito' },
+//         { email: 'solomon@topmail.net', dish: 'Burger' },
+//         { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//         { email: 'solomon@topmail.net', dish: 'Apple pie' },
+//         { email: 'jacob@hotmail.com', dish: 'Taco' },
+//     ],
+
+//     getOrdersLog() {
+//         return this.orders
+//             .map(order => `email: ${order.email} dish: ${order.dish}`)
+//             .join(' - ');
+//     },
+//     getEmails() {
+//         const emails = this.orders.map(order => order.email);
+//         const uniqueEmails = new Set(emails);
+//         console.log(uniqueEmails);
+//         return [...uniqueEmails];
+//     },
+//     getOrdersByEmail(email) {
+//         return this.orders.filter(order => order.email === email);
+//     },
+// };
+
+// console.log(historyService.getOrdersLog());
+// console.log(historyService.getEmails());
+// console.log(historyService.getOrdersByEmail('solomon@topmail.net'));
+// console.log(historyService.getOrdersByEmail('artemis@coldmail.net'));
+
+//-------------------------------------------------------------------------------------------------------------------
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+import './js/counter.js';
 // import './js/module01.js';
 // import './js/module02.js';
 // import './js/module03.js';
 // import './js/module04.js';
 import './js/module05.js';
-import './js/counter.js';
